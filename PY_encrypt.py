89f1e65653d47271fb0702a8a500c21aca9da240
mgreen01 <morgangreen0681@outlook.com>
2021-11-10 21:13:06 +0000

    CTR test code

---
+++ PY_encrypt.py
@@ -0,0 +1,13 @@
+import json
+from base64 import b64encode
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+data = b"secret"
+key = get_random_bytes(16)
+cipher = AES.new(key, AES.MODE_CTR)
+ct_bytes = cipher.encrypt(data)
+nonce = b64encode(cipher.nonce).decode('utf-8')
+ct = b64encode(ct_bytes).decode('utf-8')
+result=json.dumps({'nonce' :nonce, 'ciphertext':ct})
+print(result)
