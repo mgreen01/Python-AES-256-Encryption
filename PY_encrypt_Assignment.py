9c774896dab54ebdd06208d3e730ac1f2fd4582b
Morgan Green <morgangreen@pop-os.localdomain>
2021-11-12 17:57:07 +0000

    Decryption

--- PY_encrypt_Assignment.py
+++ PY_encrypt_Assignment.py
@@ -1,4 +1,16 @@
+#PyCryptodome library import
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 
-key = b''
+#16 byte * 8 = 128-bit Encryption Key
+key = b'mysecretpassword'
+#Encryption Cipher and Encryption Mode = Cipher Block Chaining
+cipher = AES.new(key,AES.MODE_CBC)
+#Converts plaintext to bytes for encryption
+plaintext = b'this is a message'
+#The pad variable pads the plaintext into multiples of 128-bits
+ciphertext = cipher.encrypt(pad(plaintext,AES.block_size))
+#Saves the ciphertext and the padding to make sure that the cipher text fits wihtin the 128-bit limit
+with open('cipher_file', 'wb') as c_file:
+	c_file.write(cipher.iv)
+	c_file.write(ciphertext)
