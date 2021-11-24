c81a4cdfb36acc30784e0042f8dc77ecf730fa92
Morgan Green <mgreen01@github.com>
2021-11-24 12:10:28 +0000

    Comment Rewrite

--- PY_encrypt_Assignment.py
+++ PY_encrypt_Assignment.py
@@ -2,26 +2,20 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
-plaintext
+plaintext_input=input("Please enter your plaintext:")
 plaintext = b'this is a message'
-#32 byte * 8 = 256-bit Encryption Key
+#32 byte * 8 = 256-bit Encryption Key this key is generate by the get_random_bytes from PyCryptdome
 key = get_random_bytes(32)
-#Encryption Cipher and Encryption Mode = Encrypt then authenticate then translate (EAX_mode
+#Encryption Cipher and Encryption Mode = Encrypt then authenticate then translate (EAX mode)
 cipher = AES.new(key,AES.MODE_EAX)
-ciphertext, tag = cipher.encrypt_and_digest(plaintext)
-#Converts plaintext to bytes for encryption
-
-#The pad variable pads the plaintext into multiples of 128-bits
-
-#Saves the ciphertext and the padding to make sure that the cipher text fits wihtin the 128-bit limit
-
+ciphertext, tag = cipher.encrypt_and_digest(plaintext)n
 with open("key.bin","wb")as c_file:
 	c_file.write(key)
 
 
 
 
-
+#
 file_out = open ("encryptedfile.bin", "wb")
 [ file_out.write(x) for x in (cipher.nonce, tag, ciphertext) ]
 file_out.close()
