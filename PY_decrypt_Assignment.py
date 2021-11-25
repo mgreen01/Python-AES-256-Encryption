47131f65b0e0877d331823129ee1d5903a967b9e
mgreen01 <morgangreen0681@outlook.com>
2021-11-25 11:22:42 +0000

    Comments Add

--- PY_decrypt_Assignment.py
+++ PY_decrypt_Assignment.py
@@ -1,10 +1,15 @@
+#AES PyCryptodome Library Import
 from Crypto.Cipher import AES
+#Opens the cryptotext binary and arranges in a format suitable for the program to decrypt the text.
 file_in = open("encryptedfile.bin", "rb")
 nonce, tag, ciphertext = [ file_in.read(x) for x in (16, 16, -1)]
-
+#Opens the encryption key from the encryption key binary and inserts that key into 
 with open("key.bin", "rb") as c_file:
 	key = c_file.read()
-	
 cipher = AES.new(key, AES.MODE_EAX, nonce)
 data = cipher.decrypt_and_verify(ciphertext, tag)
+#decodes the decrypted into UTF-8 encoded text
 print(data.decode('UTF-8'))
+
+#Sources for Both Encryption and Decryption Program: Chapter 6 Files and Exceptions from Starting Out with Python (4th edn) by Tony Gaddis, PyCryptodome Documentation
+
--- PY_encrypt_Assignment.py
+++ PY_encrypt_Assignment.py
@@ -1,21 +1,21 @@
 #PyCryptodome library import
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-
+#The user inputs the plaintext
 plaintext_input=input("Please enter your plaintext:")
-plaintext = b'this is a message'
+#The string input gets converted to bytes 
+plaintext = bytes(plaintext_input, 'utf-8')
 #32 byte * 8 = 256-bit Encryption Key this key is generate by the get_random_bytes from PyCryptdome
 key = get_random_bytes(32)
 #Encryption Cipher and Encryption Mode = Encrypt then authenticate then translate (EAX mode)
 cipher = AES.new(key,AES.MODE_EAX)
-ciphertext, tag = cipher.encrypt_and_digest(plaintext)n
+ciphertext, tag = cipher.encrypt_and_digest(plaintext)
+#The pseudorandomly generated key is saved on to the key binary file.
 with open("key.bin","wb")as c_file:
 	c_file.write(key)
-
-
-
-
-#
+#The cipher text, cipher nonce and the tag is saved on to the encryptedfile binary to be decrypted by the decryption program
 file_out = open ("encryptedfile.bin", "wb")
 [ file_out.write(x) for x in (cipher.nonce, tag, ciphertext) ]
 file_out.close()
+
+#Sources for Both Encryption and Decryption Program: Chapter 6 Files and Exceptions from Starting Out with Python (4th edn) by Tony Gaddis, PyCryptDome Documentation
